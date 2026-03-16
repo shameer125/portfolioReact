8f5199461354015b9065ca8df55acfd75546778c
shameer125 <alishameer251@gmail.com>
2026-03-16 11:43:23 -0700

    fix resume link

--- src/components/About/ResumeButton.jsx
+++ src/components/About/ResumeButton.jsx
@@ -1,11 +1,10 @@
 import React from "react";
 import { FaAngleRight } from "react-icons/fa";
-import resume from '../../../public/my_CV.pdf'
 
 const ResumeButton = () => {
   return (
     <a
-      href={resume}
+      href="/my_CV.pdf"
       download
       className="flex items-center inline-flex py-1.5 px-1.5 gap-1 bg-blue-800 cursor-pointer text-white px-8 py-3 rounded-xl text-lg font-medium shadow-lg hover:scale-105 transition"
     >
